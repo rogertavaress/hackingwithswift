0fed6277f715c3e5342af884a156a5123c416a49
Rogério Tavares <rogerioctf@gmail.com>
2021-10-13 21:35:16 -0300

    first commit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Rogério Tavares on 13/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+    
+    var body: some View {
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("You chose: Student # \(students[selectedStudent])")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rogério Tavares on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
