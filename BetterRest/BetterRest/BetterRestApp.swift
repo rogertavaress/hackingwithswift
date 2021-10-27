c9d780f00bd7e80b22b06735ee768b963a7c7aea
Rogério Tavares <rogerioctf@gmail.com>
2021-10-27 08:56:55 -0300

    criando o projeto BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rogério Tavares on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
