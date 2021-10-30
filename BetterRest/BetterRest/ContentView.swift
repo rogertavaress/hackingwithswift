c4a367f3afaa5ef288476c94acb4e05eb47efc53
Rogério Tavares <rogerioctf@gmail.com>
2021-10-30 14:07:50 -0300

    aprendendo sobre CoreML

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Stepper("\(sleepAmount.formatted()) horas", value: $sleepAmount, in: 4...12, step: 0.25)
+                .padding()
+            DatePicker("Insira uma data", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+        }.padding()
     }
 }
 
