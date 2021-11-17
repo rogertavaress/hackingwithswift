d9c8f539f1f6f20dcede55e83050231a04c92a0b
Rogério Tavares <rogerioctf@gmail.com>
2021-11-17 09:09:12 -0300

    iniciando o Practice Multiplication Table

---
+++ Practice Multiplication Tables/Practice Multiplication Tables/Practice_Multiplication_TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Multiplication_TablesApp.swift
+//  Practice Multiplication Tables
+//
+//  Created by Rogério Tavares on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Multiplication_TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
