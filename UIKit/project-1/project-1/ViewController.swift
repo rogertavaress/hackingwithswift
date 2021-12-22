f6f34f8d706caeb745fcb5891ad3e590c45224b3
Rogério Tavares <rogerioctf@gmail.com>
2021-12-22 20:21:30 -0300

    dia 16

---
+++ UIKit/project-1/project-1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  project-1
+//
+//  Created by Rogério Tavares on 22/12/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let itens = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in itens {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+}
+
